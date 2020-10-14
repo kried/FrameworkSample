6eefaf3ef6d31589bc3e197797a070e531d32ef5
kried <kried.evad@gmail.com>
2020-10-14 23:53:40 +0300

    update modulemap

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "FrameworkSample",
-            targets: ["FrameworkSampleWrapper"]),
+            targets: ["FrameworkSampleWrapper", "FrameworkSample"]),
     ],
     targets: [
         .target(
